86dfee7c74c7a4ce3e310612fb7e4dec34c586c9
juanalvarez11-arch <juanalvarez11@gmail.com>
2025-11-29 06:57:41 +0000

    feat: initial backend setup and activity API endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,8 +28,14 @@ SECRET_KEY = 'django-insecure-21lmm(vfgq^*%mf-=)!xcq2r8hl96lp=^rlo=sjcphye5ag4li
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow localhost and Codespace URL
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
